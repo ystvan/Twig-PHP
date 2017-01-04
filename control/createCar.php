887cd138815a20ac8f4537795cc5f2e4dab01813
Istvan Marki <istv0050@edu.easj.dk>
2017-01-04 18:34:20 +0100

    new files added

---
+++ control/createCar.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('../view');
+$twig = new Twig_Environment($loader, array('auto_reload' => true));
+
+$template = $twig -> loadTemplate('showAll.html.twig');
+
+
+//from user's input:
+$car = new stdClass();
+$car->Model = $_POST['model'];
+$car->Year = $_POST['year'];
+$car->Price = $_POST['price'];
+
+$URI = 'http://demosoap.azurewebsites.net/ServiceCarSOAP.svc?wsdl';
+$SOAP = new SoapClient($URI);
+
+$params = array('car' =>$car);
+//print_r($params);
+
+//Call wsdl function
+$res = $SOAP -> AddCar($params);
+
+//print_r($res);
+
+$twigContent = array("cars" => $res -> AddCarResult -> Car);
+print_r($twigContent);
+
+echo $template -> render($twigContent);
